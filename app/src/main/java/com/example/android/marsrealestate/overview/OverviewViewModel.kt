66e454afe9043365b2e49e94cc92f409ed35dfb1
vanh <vanhphom@gmail.com>
2020-07-04 02:22:13 -0700

    set up system to access internet

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,6 +20,15 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+//import okhttp3.Call
+//import okhttp3.Callback
+//import okhttp3.Response
+import java.net.CacheResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -44,6 +53,18 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
+
+        // enqueue all call through the Net
+        MarsApi.retrofitService.getProperties().enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+        })
         _response.value = "Set the Mars API Response here!"
     }
 }
+
